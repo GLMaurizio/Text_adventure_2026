2ad61e741f7b2126a5d833f86324d01f69c2bd4f
addictivqe <groovygallaxy@gmail.com>
2026-01-07 11:20:06 +0100

    Add health field to Player class

--- Module_4/Code/Zuul/Zuul/classes/Player.cs
+++ Module_4/Code/Zuul/Zuul/classes/Player.cs
@@ -2,10 +2,16 @@ class Player
 { 
     // auto property
     public Room CurrentRoom { get; set; }
+
+    // Fields
+    private int health;
+
     // constructor
     public Player()
     {
         CurrentRoom = null;
+
+        health = 100;
     }
 
     
